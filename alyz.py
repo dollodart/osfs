5c154660d4504c7b7ddfe28f66c624391467a8fb
David Ollodart <davidollodart@gmail.com>
2022-10-15 18:17:24 -0700

    refactoring the report generation
    this is to enable the analytics to be run on the in memory filesystem
    generated by methods other than load_app_memory

--- alyz.py
+++ alyz.py
@@ -5,21 +5,30 @@ from dfs import dfs
 from lca import least_common_ancestor
 from time import time
 
-def alyz_fs(root_str):
+def load_app_memory_subroutine(root_str):
     # load into object relational mapping
     t0 = time()
     root = load_app_memory(root_str)
+    return t0, root
+
+def dfs_subroutine(root):
     #dfs
     t1 = time()
     # fictitious root lets you use the "subtree depths" to group any variable by depth
-    fictitious_root = Directory(None, None, root.st)
+    fictitious_root = Directory(None, '', root.st)
     fictitious_root.add_child(root) 
     accs1, P = dfs(fictitious_root)
     for k in 'ndir', 'nsym', 'nrf', 'nchild':
         accs1[k].pop(0)
+    return t1, accs1, P
+
+def lca_subroutine(root, P):
     # lca
     t2 = time()
     accs2 = least_common_ancestor(root, P)
+    return t2, accs2
+
+def analytics_subroutine(accs1, accs2, P):
     # analytics
     t3 = time()
     print('link type', 'count')
@@ -55,13 +64,18 @@ def alyz_fs(root_str):
     print(gr.round(2))
     print('mean(std/mean) for branch factor is', f"{std_o_mean.mean():.2f}")
     t4 = time()
+    return t3
     #
+if __name__ == '__main__':
+    rootstrs = ('./fs-ex', '/etc', '/usr', '/lib')
+    t0, root = load_app_memory_subroutine(rootstrs[1])
+    t1, accs1, P = dfs_subroutine(root)
+    t2, accs2 = lca_subroutine(root, P)
+    t3 = analytics_subroutine(accs1, accs2, P)
+    t4 = time()
+
     print('times')
     print(f'process={t1-t0:.3f}\n' +
           f'dfs={t2-t1:.3f}\n' +
           f'lca={t3-t2:.3f}\n' + 
           f'analytics={t4-t3:.3f}')
-
-if __name__ == '__main__':
-    rootstrs = ('./example', '/etc', '/usr', '/lib')
-    alyz_fs(rootstrs[1])
